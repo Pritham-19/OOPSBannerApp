c8e0161fb38baab12121e338d8226499ba6bf33d
Pritham <pm6408@srmist.edu.in>
2026-03-11 13:24:55 +0530

    UC7 Inner class

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,71 +1,114 @@
 public class OOPSBannerApp {
 
-    // Pattern for O
-    public static String[] getOPattern() {
-        return new String[]{
-                " *** ",
-                "*   *",
-                "*   *",
-                "*   *",
-                "*   *",
-                "*   *",
-                " *** "
-        };
-    }
-	 // Pattern for O
-    public static String[] getOPattern() {
-        return new String[]{
-                " *** ",
-                "*   *",
-                "*   *",
-                "*   *",
-                "*   *",
-                "*   *",
-                " *** "
-        };
+    // Inner Class
+    static class CharacterPatternMap {
+        char character;
+        String[] pattern;
+
+        // Constructor
+        CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        // Getter
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
     }
 
-    // Pattern for P
-    public static String[] getPPattern() {
-        return new String[]{
-                "**** ",
+    // Create pattern map
+    public static CharacterPatternMap[] createCharacterPatternMaps() {
+
+        CharacterPatternMap[] maps = new CharacterPatternMap[4];
+
+        maps[0] = new CharacterPatternMap('O', new String[]{
+                " *** ",
                 "*   *",
                 "*   *",
+                "*   *",
+                " *** "
+        });
+
+        maps[1] = new CharacterPatternMap('P', new String[]{
                 "**** ",
-                "*    ",
+                "*   *",
+                "**** ",
                 "*    ",
                 "*    "
-        };
-    }
+        });
 
-    // Pattern for S
-    public static String[] getSPattern() {
-        return new String[]{
+        maps[2] = new CharacterPatternMap('S', new String[]{
                 " ****",
-                "*    ",
                 "*    ",
                 " *** ",
                 "    *",
-                "    *",
                 "**** "
+        });
+
+        maps[3] = new CharacterPatternMap(' ', new String[]{
+                "  ",
+                "  ",
+                "  ",
+                "  ",
+                "  "
+        });
+
+        return maps;
+    }
+
+
+    // Get pattern
+    public static String[] getCharacterPattern(char ch,
+                                               CharacterPatternMap[] maps) {
+
+        for (CharacterPatternMap m : maps) {
+            if (m.getCharacter() == ch) {
+                return m.getPattern();
+            }
+        }
+
+        return new String[]{
+                "????",
+                "????",
+                "????",
+                "????",
+                "????"
         };
     }
 
-    // Main method
+
+    // Print message
+    public static void printMessage(String message,
+                                    CharacterPatternMap[] maps) {
+
+        int height = 5;
+
+        for (int i = 0; i < height; i++) {
+
+            for (int j = 0; j < message.length(); j++) {
+
+                String[] pattern =
+                        getCharacterPattern(message.charAt(j), maps);
+
+                System.out.print(pattern[i] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+
+    // Main
     public static void main(String[] args) {
 
-        String[] oPattern = getOPattern();
-        String[] pPattern = getPPattern();
-        String[] sPattern = getSPattern();
+        CharacterPatternMap[] maps =
+                createCharacterPatternMaps();
 
-        // Print OOPS
-        for (int i = 0; i < oPattern.length; i++) {
-            System.out.println(
-                    oPattern[i] + "  " +
-                    oPattern[i] + "  " +
-                    pPattern[i] + "  " +
-                    sPattern[i]
-            );
-        }
+        printMessage("OOPS", maps);
     }
 }
